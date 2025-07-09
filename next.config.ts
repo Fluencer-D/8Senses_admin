cecb4fbfe1f020ba6a86c830992e6260dcc9a13f
satishnadipalli <satishnadipalli1@gmail.com>
2025-07-09 21:25:28 +0530

    updateed next-config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  basePath: "/admin",
+  assetPrefix: '/admin',
+  trailingSlash: true,
+  experimental: {
+    serverActions: true,
+    outputFileTracingRoot: undefined,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
+    unoptimized: true,
     domains: ["res.cloudinary.com"],
   },
 };
 
-
-export default nextConfig;
+module.exports = nextConfig;
