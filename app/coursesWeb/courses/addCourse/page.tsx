5bf2e2d78f375f0097ecf5c50cc380a87c226fef
satishnadipalli <satishnadipalli1@gmail.com>
2025-09-11 14:26:25 +0530

    updated

--- app/coursesWeb/courses/addCourse/page.tsx
+++ app/coursesWeb/courses/addCourse/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { getAdminToken } from '@/utils/storage';
-import { useRouter } from "next/navigation"
 // Define types to match the MongoDB schema
 interface VideoData {
   title: string;
